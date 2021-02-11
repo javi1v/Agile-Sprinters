8141e8910d53c0e3a997cf52e444d28164bec416
AdminCrystal <50718608+AdminCrystal@users.noreply.github.com>
2021-02-11 15:48:27 -0700

    Brent's Sprint1 changes

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,13 +16,10 @@ namespace ProjectTemplate
 
 	public class ProjectServices : System.Web.Services.WebService
 	{
-		////////////////////////////////////////////////////////////////////////
-		///replace the values of these variables with your database credentials
-		////////////////////////////////////////////////////////////////////////
+		// Our databases credentials
 		private string dbID = "2021group4";
 		private string dbPass = "group42021";
 		private string dbName = "2021group4";
-		////////////////////////////////////////////////////////////////////////
 		
 		////////////////////////////////////////////////////////////////////////
 		///call this method anywhere that you need the connection string!
@@ -30,26 +27,16 @@ namespace ProjectTemplate
 		private string getConString() {
 			return "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName+"; UID=" + dbID + "; PASSWORD=" + dbPass;
 		}
-		////////////////////////////////////////////////////////////////////////
+		
 
-
-
-		/////////////////////////////////////////////////////////////////////////
-		//don't forget to include this decoration above each method that you want
-		//to be exposed as a web service!
 		[WebMethod(EnableSession = true)]
-		/////////////////////////////////////////////////////////////////////////
 		public string TestConnection()
 		{
 			try
 			{
 				string testQuery = "select * from Users";
-
-				////////////////////////////////////////////////////////////////////////
-				///here's an example of using the getConString method!
-				////////////////////////////////////////////////////////////////////////
+				// Connects to the database
 				MySqlConnection con = new MySqlConnection(getConString());
-				////////////////////////////////////////////////////////////////////////
 
 				MySqlCommand cmd = new MySqlCommand(testQuery, con);
 				MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
@@ -61,6 +48,113 @@ namespace ProjectTemplate
 			{
 				return "Something went wrong, please check your credentials and db name and try again.  Error: "+e.Message;
 			}
+
+
 		}
+
+		[WebMethod]
+		public int NumberOfAccounts()
+		{
+			// A simple query that will need to be updated when size of database is larger
+			string sqlSelect = "SELECT * from Users";
+
+			//set up our connection object to be ready to use our connection string
+			MySqlConnection sqlConnection = new MySqlConnection(getConString());
+			//set up our command object to use our connection, and our query
+			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+
+			//a data adapter acts like a bridge between our command object and 
+			//the data we are trying to get back and put in a table object
+			MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+			//here's the table we want to fill with the results from our query
+			DataTable sqlDt = new DataTable();
+			//here we go filling it!
+			sqlDa.Fill(sqlDt);
+			//return the number of rows we have, that's how many accounts are in the system!
+			return sqlDt.Rows.Count;
+		}
+
+		[WebMethod]
+		public bool LogOn(string email, string password)
+		{
+			// Returns this value to say whether or not the log on was succesful
+			bool wasSuccesful = false;
+
+			// A basic select query that uses the @ symbol for safety
+			string sqlSelect = "SELECT EmployeeId FROM Users WHERE Email=@idValue and Password=@passValue";
+
+			// Connects us to the database
+			MySqlConnection sqlConnection = new MySqlConnection(getConString());
+			// Create new object to send the select statement to the database connection
+			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+			//tell our command to replace the @parameters with the actual values
+			//we decode them because they had to be encoded to be sent over the web
+			sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(email));
+			sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(password));
+
+			//a data adapter acts like a bridge between our command object and 
+			//the data we are trying to get back and put in a table object
+			MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+			//here's the table we want to fill with the results from our query
+			DataTable sqlDt = new DataTable();
+			//here we go filling it!
+			sqlDa.Fill(sqlDt);
+			//check to see if any rows were returned.  If they were, it means it's 
+			//a legit account
+			if (sqlDt.Rows.Count > 0)
+			{
+				//flip our flag to true so we return a value that lets them know they're logged in
+				wasSuccesful = true;
+			}
+
+			//return the result!
+			return wasSuccesful;
+		}
+
+		[WebMethod]
+		public String SeeAccountNames()
+		{
+
+			// Stores all first and last names
+			String accounts = "";
+			
+			// This query gets all the first and last names
+			string sqlSelect = "SELECT FirstName, LastName FROM Users";
+
+			//set up our connection object to be ready to use our connection string
+			MySqlConnection sqlConnection = new MySqlConnection(getConString());
+			//set up our command object to use our connection, and our query
+			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+			//a data adapter acts like a bridge between our command object and 
+			//the data we are trying to get back and put in a table object
+			MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+			//here's the table we want to fill with the results from our query
+			DataTable sqlDt = new DataTable();
+			//here we go filling it!
+			sqlDa.Fill(sqlDt);
+
+			// Formats the list of names
+			foreach (DataRow row in sqlDt.Rows)
+			{
+				accounts += row[0];
+				accounts += " " + row[1];
+				accounts += ", ";
+			}
+
+			// returns all the names
+			return accounts;
+		}
+
+		[WebMethod]
+		public bool LogOff()
+        {
+			// Can not log out of the system until their is a true log off feature
+			// Very temporary return true statement
+			return true;
+        }
+
 	}
 }
