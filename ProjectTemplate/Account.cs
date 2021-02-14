b6476eef59d1268b73354115381c3f6ff892453b
admincrystal <mr.brent.hansen@gmail.com>
2021-02-13 17:35:49 -0700

    Json Dependencies

---
+++ ProjectTemplate/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class Account
+    {
+		//this is just a container for all info related
+		//to an account.  We'll simply create public class-level
+		//variables representing each piece of information!
+		public int EmployeeId;
+		public string Password;
+		public string Email;
+		public string FirstName;
+		public string LastName;
+		public string PhoneNumber;
+		public string Title;
+	}
+}
