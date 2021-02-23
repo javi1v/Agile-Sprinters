21249541bd83d89390a4601658ecad498c0434e3
brent hansen <mr.brent.hansen@gmail.com>
2021-02-23 09:59:38 -0700

    Message functionality, true log in and off

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -60,18 +60,17 @@ namespace ProjectTemplate
 		}
 
 
-		[WebMethod]
+		[WebMethod(EnableSession = true)]
 		public bool LogOn(string email, string password)
 		{
 			// Returns this value to say whether or not the log on was succesful
 			bool wasSuccesful = false;
 
 			// A basic select query that uses the @ symbol for safety
-			string sqlSelect = "SELECT EmployeeId FROM Users WHERE Email=@idValue and Password=@passValue";
+			string sqlSelect = "SELECT EmployeeId, Title FROM Users WHERE Email=@idValue and Password=@passValue";
 
 			// Connects us to the database
 			MySqlConnection sqlConnection = new MySqlConnection(getConString());
-			// Create new object to send the select statement to the database connection
 			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
 
 			//tell our command to replace the @parameters with the actual values
@@ -84,13 +83,15 @@ namespace ProjectTemplate
 			MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
 			//here's the table we want to fill with the results from our query
 			DataTable sqlDt = new DataTable();
-			//here we go filling it!
+			
 			sqlDa.Fill(sqlDt);
-			//check to see if any rows were returned.  If they were, it means it's 
-			//a legit account
+			
+			// If something was returned then the log on was successful
 			if (sqlDt.Rows.Count > 0)
 			{
-				//flip our flag to true so we return a value that lets them know they're logged in
+				//Returns true if it was succesful and creates a sesion id for them
+				Session["id"] = sqlDt.Rows[0]["EmployeeId"];
+				Session["admin"] = sqlDt.Rows[0]["Title"];
 				wasSuccesful = true;
 			}
 
@@ -99,7 +100,7 @@ namespace ProjectTemplate
 		}
 
 
-		[WebMethod]
+		[WebMethod(EnableSession = true)]
 		public Account[] GetAccounts()
 		{
 			// TODO Check if a user is logged in
@@ -191,14 +192,100 @@ namespace ProjectTemplate
 		}
 
 
-		[WebMethod]
+		[WebMethod(EnableSession = true)]
 		public bool LogOff()
 		{
-			// Can not log out of the system until their is a true log on feature
-			// Very temporary return true statement
+			// This will make the user relog in, in order to access the services
+			// This basically removes the cookie that identifies the user
+			Session.Abandon();
 			return true;
 		}
 
+
+		[WebMethod(EnableSession = true)]
+		public void RequestAccount(string Email, string Password, string FirstName, string LastName, string PhoneNumber, string Title)
+		{
+			//the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
+			//does is tell mySql server to return the primary key of the last inserted row.
+			string sqlSelect = "insert into Users (Password, Email, FirstName, LastName, PhoneNumber, Title) " +
+				"values(@Password, @Email, @FirstName, @LastName, @PhoneNumber, @Title); SELECT LAST_INSERT_ID();";
+
+			MySqlConnection sqlConnection = new MySqlConnection(getConString());
+			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+			sqlCommand.Parameters.AddWithValue("@Email", HttpUtility.UrlDecode(Email));
+			sqlCommand.Parameters.AddWithValue("@Password", HttpUtility.UrlDecode(Password));
+			sqlCommand.Parameters.AddWithValue("@FirstName", HttpUtility.UrlDecode(FirstName));
+			sqlCommand.Parameters.AddWithValue("@LastName", HttpUtility.UrlDecode(LastName));
+			sqlCommand.Parameters.AddWithValue("@PhoneNumber", HttpUtility.UrlDecode(PhoneNumber));
+			sqlCommand.Parameters.AddWithValue("@Title", HttpUtility.UrlDecode(Title));
+
+			//this time, we're not using a data adapter to fill a data table.  We're just
+			//opening the connection, telling our command to "executescalar" which says basically
+			//execute the query and just hand me back the number the query returns (the ID, remember?).
+			//don't forget to close the connection!
+			sqlConnection.Open();
+			//we're using a try/catch so that if the query errors out we can handle it gracefully
+			//by closing the connection and moving on
+			try
+			{
+				int accountID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+				//here, you could use this accountID for additional queries regarding
+				//the requested account.  Really this is just an example to show you
+				//a query where you get the primary key of the inserted row back from
+				//the database!
+			}
+			catch (Exception e)
+			{
+			}
+			sqlConnection.Close();
+		}
+
+
+		[WebMethod(EnableSession = true)]
+		public void SendMessage(string EmployeeEmailToSendTo, string Subject, string Message)
+        {
+			string sqlMessage = "Insert into Messages (MessageId, From_id, To_id, Subject, Message, timesent, wasread) values(66, @fromEmail, @toEmail, @Subject, @Message, Now(), false)";
+			string sqlGetEmail = "Select Email from Users where EmployeeId = @EmployeeId";
+			string currentEmailAddress = "";
+
+			MySqlConnection sqlConnection = new MySqlConnection(getConString());
+			//set up our command object to use our connection, and our query
+			MySqlCommand emailCommand = new MySqlCommand(sqlGetEmail, sqlConnection);
+			emailCommand.Parameters.AddWithValue("@EmployeeId", Session["id"]);
+
+			MySqlDataAdapter sqlDa = new MySqlDataAdapter(emailCommand);
+			//here's the table we want to fill with the results from our query
+			DataTable sqlDt = new DataTable();
+			//here we go filling it!
+			sqlDa.Fill(sqlDt);
+			//check to see if any rows were returned.  If they were, it means it's 
+			//a legit account
+			if (sqlDt.Rows.Count > 0)
+			{
+				currentEmailAddress = sqlDt.Rows[0]["Email"].ToString();
+				
+			}
+			
+			MySqlConnection connection = new MySqlConnection(getConString());
+			//set up our command object to use our connection, and our query
+			MySqlCommand messageCommand = new MySqlCommand(sqlMessage, connection);
+			messageCommand.Parameters.AddWithValue("@fromEmail", currentEmailAddress);
+			messageCommand.Parameters.AddWithValue("@toEmail", EmployeeEmailToSendTo);
+			messageCommand.Parameters.AddWithValue("@Subject", Subject);
+			messageCommand.Parameters.AddWithValue("@Message", Message);
+
+			connection.Open();
+			try
+			{
+				messageCommand.ExecuteNonQuery();
+			}
+			catch (Exception e)
+			{
+			}
+			connection.Close();
+
+		}
 
 		[WebMethod]
 		public void DeleteUser(string adminEmail, string adminPassword, int EmployeeIdToDelete)
